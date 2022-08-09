b20a745abc9351ab3ba2eb485ca862c171dbb1e8
Urek Mazino <urek@localhost.localdomain>
2022-08-09 13:15:40 +0530

    CORS error for guest search solved.

--- src/app.js
+++ src/app.js
@@ -14,15 +14,18 @@ app.use(workerRouter)
 app.use(userRouter)
 
 //CORS Configuration
-app.use(cros())
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+const corsOpts = {
+    origin: '*',
+    methods: [
+      'GET',
+      'POST',
+    ],
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+  
+app.use(cors(corsOpts));
 
 
 //------------------------Guest Routes---------------------------

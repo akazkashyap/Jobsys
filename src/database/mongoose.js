5b154a292fdd7380bdf5e116bc3f3c6fb59b1c84
Urek Mazino <urek@localhost.localdomain>
2022-10-15 15:33:53 +0530

    cant connect to db fixed

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-// process.env.MONGODB_URL;
+// ;
 // const MONGODB_URL = "mongodb://127.0.0.1:27017/worker_api"
 
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
 }).catch((error) => {
     console.log("Unable to connect to database.")

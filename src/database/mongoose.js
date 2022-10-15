57fc90fa8acf52fbb74526b4b37a26b9677a6c1a
Urek Mazino <urek@localhost.localdomain>
2022-10-15 15:32:30 +0530

    signup no email error fixed

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-process.env.MONGODB_URL;
-const MONGODB_URL = "mongodb://127.0.0.1:27017/worker_api"
+// process.env.MONGODB_URL;
+// const MONGODB_URL = "mongodb://127.0.0.1:27017/worker_api"
 
 mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
